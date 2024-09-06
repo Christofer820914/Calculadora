89a81c0e4612289b18ad1837a96b1f3f9e26cb76
hp835 <hp835@host.docker.internal>
2024-09-06 17:49:43 -0600

    Metodo de suma gnerico con test corregido

---
+++ src/main/java/mx/com/qtx/calc/servicios/Calculadora.java
@@ -0,0 +1,9 @@
+package mx.com.qtx.calc.servicios;
+
+public class Calculadora {
+	public <T extends Number> double sumar (T x, T y) {
+		double suma = x.doubleValue() + y.doubleValue();
+		return suma;
+	}
+
+}
---
+++ src/main/java/mx/com/qtx/calc/tests/CalculadoraTests.java
@@ -0,0 +1,10 @@
+package mx.com.qtx.calc.tests;
+
+public class CalculadoraTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
